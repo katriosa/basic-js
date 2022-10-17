1c936849a39ae94542a45bf560db3b57ecf056a0
katriosa <ee.chistyakova@gmail.com>
2022-10-17 22:58:03 +0400

    feat: transform array - done

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,23 +1,54 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Create transformed array based on the control sequences that original
  * array contains
- * 
+ *
  * @param {Array} arr initial array
  * @returns {Array} transformed array
- * 
+ *
  * @example
- * 
+ *
  * transform([1, 2, 3, '--double-next', 4, 5]) => [1, 2, 3, 4, 4, 5]
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
- * 
+ *
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
 
+ function transform(arr) {
+  if (!Array.isArray(arr))
+    throw new Error("'arr' parameter must be an instance of the Array!");
+
+  const arrCopy = arr.length > 0 ? arr.concat() : arr;
+
+  arr.forEach((el, index) => {
+    let currentIndex = arrCopy.indexOf(el);
+    switch (el) {
+      case "--discard-next":
+        arrCopy[index + 1]
+          ? arrCopy.splice(index, 2, undefined)
+          : arrCopy.splice(index, 1);
+        return;
+      case "--discard-prev":
+        arrCopy[currentIndex - 1]
+          ? arrCopy.splice(currentIndex - 1, 2, undefined)
+          : arrCopy.splice(currentIndex, 1);
+        return;
+      case "--double-next":
+        arrCopy[index + 1]
+          ? arrCopy.splice(index, 1, arrCopy[index + 1])
+          : arrCopy.splice(index, 1);
+        return;
+      case "--double-prev":
+        arrCopy[currentIndex - 1]
+          ? arrCopy.splice(currentIndex, 1, arrCopy[currentIndex - 1])
+          : arrCopy.splice(currentIndex, 1);
+        return;
+      default:
+        return;
+    }
+  });
+  return arrCopy.filter((el) => el);
+}
 module.exports = {
-  transform
+  transform,
 };
--- src/what-season.js
+++ src/what-season.js
@@ -1,21 +1,26 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Extract season from given date and expose the enemy scout!
- * 
+ *
  * @param {Date | FakeDate} date real or fake date
  * @returns {String} time of the year
- * 
+ *
  * @example
- * 
+ *
  * getSeason(new Date(2020, 02, 31)) => 'spring'
- * 
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
+function getSeason(date) {
+  throw new NotImplementedError("Not implemented");
   // remove line with error and write your code here
+  // if (isNaN(Date.parse(date))) return "Unable to determine the time of year!";
+  // let mounth = date.getMonth();
+  // if (mounth === 2 || 3 || 4) return "spring";
+  // if (mounth === 5 || 6 || 7) return "summer";
+  // if (mounth === 8 || 9 || 10) return "autumn";
+  // if (mounth === 11 || 0 || 1) return "winter";
 }
 
 module.exports = {
-  getSeason
+  getSeason,
 };
